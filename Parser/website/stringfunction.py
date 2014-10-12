c9ed9aaff47acc09f35d7b608181f0e6bec00118
iampf <iampf510@gmail.com>
2014-10-13 04:09:11 +0800

    stringfuction finish

---
+++ Parser/website/stringfunction.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+#!coding:utf-8
+
+import urllib2,urllib
+import sys
+sys.path.append('..')
+import browser_bot
+
+def getResult(cypher):
+	plantext = ''
+
+	url = 'http://www.stringfunction.com/md5-decrypter.html'
+
+
+	b = browser_bot.BOT()
+	data = b.decrypt(url,cypher, {'name':'string_md5'}, {'name':'submit'})
+	plantext = data.split('<textarea')[1].split('</textarea>')[0].split('">')[1]
+	if plantext == '':
+		return None
+	else:
+		return plantext
+
+if __name__ == '__main__':
+	p = getResult('b4b147bc522828731f1a016bfa72c073')
+	#p = getResult('3f5824a95bd646ead7b5485dabc70583')
+	print p
